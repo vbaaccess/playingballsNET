818c626034851278f8249b3717733a31abccc1ba
koter <42820377+vbaaccess@users.noreply.github.com>
2019-11-24 12:07:20 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+
+namespace playingballsNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello in Game!");
+        }
+    }
+}
