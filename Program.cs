facea1f97ae58ddf60f18df85beafd83c326ff2f
pablodd <pablodd@wp.pl>
2019-11-24 15:26:26 +0100

    ferst commit

--- Program.cs
+++ Program.cs
@@ -7,7 +7,16 @@ namespace playingballsNET
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello in Game!");
+            Console.WriteLine("Hello in Game!");                        //abaut game
+            Console.WriteLine("1.1) select paramieters");               //TO DO
+            Console.WriteLine("1.2) start game");
+            Console.WriteLine("1.3) exit game");
+            Console.WriteLine("2.1) raound 3 new ball and show");
+            Console.WriteLine("2.2) put 3 ball on board");
+            Console.WriteLine("3.1) select color (ball)");
+            Console.WriteLine("3.2) move target field");
+            Console.WriteLine("3.3) move ball (lewenstain algorytm)");
+            Console.WriteLine("3.4) ad point to player");
         }
     }
 }
