7796d674fb92d0e3c5c5ee423f7b6064bc74d8c5
koter <42820377+vbaaccess@users.noreply.github.com>
2019-12-02 16:32:29 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,11 +8,11 @@ namespace playingballsNET
         static void Main(string[] args)
         {
             Console.WriteLine("Hello in Game!");                        //abaut game
-            Console.WriteLine("configure repo on github");              //TO DO
+            Console.WriteLine("configure repo on github");
             Console.WriteLine("1.1) select paramieters");               //TO DO
             Console.WriteLine("1.2) start game");
             Console.WriteLine("1.3) exit game");
-            Console.WriteLine("2.1) raound 3 new ball and show");
+            Console.WriteLine("2.1) raound 3 new ball and show");       //TO DO
             Console.WriteLine("2.2) put 3 ball on board");
             Console.WriteLine("3.1) select color (ball)");
             Console.WriteLine("3.2) move target field");
